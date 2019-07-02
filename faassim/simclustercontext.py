9bd587da2cc0ddf652a049fc13f7b318de2bcc4f
Alexander Rashed <alexander.rashed@gmail.com>
2019-07-02 19:28:55 +0200

    Working k8s + sim prototype

--- faassim/simclustercontext.py
+++ faassim/simclustercontext.py
@@ -1,15 +1,44 @@
-from typing import List, Dict
+from typing import List
 
 from core.clustercontext import ClusterContext
-from core.model import Node, ImageState
+from core.model import Node, ImageState, Capacity
 
 
 class SimulationClusterContext(ClusterContext):
 
-    def retrieve_image_state(self, image_name: str) -> ImageState:
-        # TODO return the image states for the image names used by the pod synthesizer
-        pass
+    def __init__(self):
+        super().__init__()
+
+        # TODO synthesize lots of nodes based on our test-environment
+        self.nodes = [
+            Node(name='ara-clustercloud1',
+                 capacity=Capacity(cpu_millis = 4 * 1000,
+                                   memory= 8 * 1024 * 1024 * 1024),
+                 allocatable=Capacity(cpu_millis = 4 * 1000,
+                                   memory= 8 * 1024 * 1024 * 1024),
+                 labels={
+                     'beta.kubernetes.io/arch': 'amd64'
+                 }),
+            Node(name='ara-clustertegra1',
+                 capacity=Capacity(cpu_millis = 4 * 1000,
+                                   memory= 8 * 1024 * 1024 * 1024),
+                 allocatable=Capacity(cpu_millis = 4 * 1000,
+                                   memory= 8 * 1024 * 1024 * 1024),
+                 labels={
+                     'beta.kubernetes.io/arch': 'arm64',
+                     'capability.skippy.io': 'nvidia-cuda-10'
+                 })
+        ] + [
+            Node(name='ara-clusterpi{}'.format(i),
+                 capacity=Capacity(cpu_millis = 4 * 1000,
+                                   memory= 1 * 1024 * 1024 * 1024),
+                 allocatable=Capacity(cpu_millis = 4 * 1000,
+                                   memory= 1 * 1024 * 1024 * 1024),
+                 labels={
+                     'beta.kubernetes.io/arch': 'arm'
+                 })
+             for i in range(1,4)
+        ]
 
     def list_nodes(self) -> List[Node]:
-        # TODO
-        return [Node('test')]
+        return self.nodes
--- faassim/synth.py
+++ faassim/synth.py
@@ -1,15 +1,17 @@
 from typing import Generator
 
-from core.model import Pod, PodSpec
+from core.model import Pod, PodSpec, Container, ResourceRequirements
 
 PodSynthesizer = Generator[Pod, None, None]
 
 
 def pod_synthesizer() -> PodSynthesizer:
-    # TODO: synthesize parameters, add function specific annotations,...
+    # TODO: synthesize parameters, add function specific annotations based on our scenario
     cnt = 1
     while True:
         spec = PodSpec()
+        container = Container('alexrashed/ml-wf-1-pre:0.33', ResourceRequirements())
+        spec.containers = [container]
 
         pod = Pod('pod-{0}'.format(cnt))
         cnt += 1
