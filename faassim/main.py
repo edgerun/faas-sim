65682eee3d40dad75265f0dad786d55d0e344a40
Alexander Rashed <alexander.rashed@gmail.com>
2019-07-16 19:12:34 +0200

    Prepare synthesis

--- faassim/main.py
+++ faassim/main.py
@@ -1,19 +1,33 @@
+import argparse
 import time
+from tkinter import EventType
+from typing import NamedTuple, Dict, List, Generator
 import simpy
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import logging
-
 from core.scheduler import Scheduler
 from sim.simclustercontext import SimulationClusterContext
 from sim.stats import exp_sampler
 from sim.synth import pod_synthesizer, PodSynthesizer
 
 
-def run_load_generator(env: simpy.Environment, queue: simpy.Store, pod_synth: PodSynthesizer, ia_sampler):
+class LoggingRow(NamedTuple):
+    timestamp: float                 # When did it happen?
+    event: EventType                 # What happened?
+    value: str                       # Which pod was affected?
+    additional_attributes: Dict = {} # What else could be interesting?
+
+
+def run_load_generator(env: simpy.Environment, queue: simpy.Store, pod_synth: PodSynthesizer,
+                       ia_sampler: Generator[float, float, None], log: List[LoggingRow]):
     """
     :param env: simpy environment
     :param queue: the work queue
     :param pod_synth: fake Pod generator
     :param ia_sampler: arrival profile
+    :param log: simple array to append log messages
     :return:
     """
     while True:
@@ -25,15 +39,17 @@ def run_load_generator(env: simpy.Environment, queue: simpy.Store, pod_synth: Po
         queue.put(pod)
 
         logging.debug('pod arrived at %.2f seconds' % env.now)
+        log.append(LoggingRow(env.now, EventType.POD_QUEUED, pod.name, {'queue_length': len(queue.items)}))
 
 
-def run_scheduler_worker(env: simpy.Environment, queue: simpy.Store, scheduler: Scheduler):
+def run_scheduler_worker(env: simpy.Environment, queue: simpy.Store, log: List[LoggingRow], scheduler: Scheduler):
     while True:
         logging.debug('Scheduler waiting for pod...')
         pod = yield queue.get()
 
         # TODO fix time not changing (env.now)
         logging.debug('Pod received by scheduler at %.2f', env.now)
+        log.append(LoggingRow(env.now, EventType.POD_RECEIVED, pod.name))
         then = time.time()
 
         # execute scheduling algorithm
@@ -42,26 +58,52 @@ def run_scheduler_worker(env: simpy.Environment, queue: simpy.Store, scheduler: 
         duration = ((time.time() - then) * 1000)
         yield env.timeout(duration / 1000)
         logging.debug('Pod scheduling took %.2f ms, and yielded %s', duration, result)
+        log.append(LoggingRow(env.now, EventType.POD_SCHEDULED, pod.name))
 
 
-def main():
-    # TODO
-    # - Properly synthesize ml-wf-workload
-    # - Make sure to measure the placement quality instead of the placement time
-    logging.getLogger().setLevel(logging.DEBUG)
-
+def simulate():
+    log = []
     cluster_context = SimulationClusterContext()
     scheduler = Scheduler(cluster_context)
 
     env = simpy.RealtimeEnvironment(factor=0.01, strict=False)
     queue = simpy.Store(env)
-    env.process(run_load_generator(env, queue, pod_synthesizer(), exp_sampler(lambd=1.5)))
-    env.process(run_scheduler_worker(env, queue, scheduler))
+    env.process(run_load_generator(env, queue, pod_synthesizer(), exp_sampler(lambd=1.5), log))
+    env.process(run_scheduler_worker(env, queue, log, scheduler))
     env.sync()
     try:
         env.run(until=200)
+        data = pd.DataFrame(data=log)
+        # TODO implement oracle to calculate execution and placement time for a selected node / config
+        #  Add the calucalated execution time and placement time via the oracle and add it to the log rows
+        # TODO save the CSV
     except KeyboardInterrupt:
         pass
+
+
+def plot():
+    # TODO plot the data
+    pass
+
+
+def main():
+    # Parse the arguments
+    parser = argparse.ArgumentParser(description='Skippy Simulator')
+    parser.add_argument('-d', '--debug', action='store_true', dest='debug',
+                        help='Enable debug logs.', default=False)
+    parser.add_argument('-s', '--simulate', action='store_true', dest='debug',
+                        help='Only simulate the scheduling.', default=False)
+    parser.add_argument('-p', '--plot', action='store_true', dest='debug',
+                        help='Only plot the data.', default=False)
+    args = parser.parse_args()
+    level = logging.DEBUG if args.debug else logging.INFO
+    logging.getLogger().setLevel(level)
+    
+    if args.simulate or not args.plot:
+        simulate()
+
+    if args.plot or not args.simulate:
+        plot()
 
 
 if __name__ == '__main__':
--- faassim/simclustercontext.py
+++ faassim/simclustercontext.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 from core.clustercontext import ClusterContext
 from core.model import Node, ImageState, Capacity
@@ -40,8 +40,98 @@ class SimulationClusterContext(ClusterContext):
              for i in range(1,5)
         ]
 
-    def get_next_storage_node(self, node: Node) -> str:
-        return 'ara-clustercloud1'
-
     def list_nodes(self) -> List[Node]:
         return self.nodes
+
+    def get_next_storage_node(self, node: Node) -> str:
+        # TODO maybe switch the storage node to different ones than the master?
+        return 'ara-clustercloud1'
+
+    def get_init_image_states(self) -> Dict[str, ImageState]:
+        # TODO maybe synth other images?
+        # https://cloud.docker.com/v2/repositories/alexrashed/ml-wf-1-pre/tags/0.33/
+        # https://cloud.docker.com/v2/repositories/alexrashed/ml-wf-2-train/tags/0.33/
+        # https://cloud.docker.com/v2/repositories/alexrashed/ml-wf-3-serve/tags/0.33/
+        return {
+            'alexrashed/ml-wf-1-pre:0.33': ImageState(size={
+                'arm': 461473086,
+                'arm64': 538015840,
+                'amd64': 530300745
+            }),
+            'alexrashed/ml-wf-2-train:0.33': ImageState(size={
+                'arm': 506029298,
+                'arm64': 582828211,
+                'amd64': 547365470
+            }),
+            'alexrashed/ml-wf-3-serve:0.33': ImageState(size={
+                'arm': 506769993,
+                'arm64': 585625232,
+                'amd64': 585928717
+            })
+        }
+
+    def get_bandwidth_graph(self) -> Dict[str, Dict[str, float]]:
+        # TODO synthesize the graph -> N^2 entries!
+        #  for all nodes in self.list_nodes()
+        # 1.25e+6 Byte/s = 10 MBit/s
+        # 1.25e+7 Byte/s = 100 MBit/s
+        # 1.25e9 Byte/s = 10 GBit/s - assumed for local access
+        # The registry is always connected with 100 MBit/s (replicated in both networks)
+        # The edge nodes are interconnected with 100 MBit/s
+        # The cloud is connected to the edge nodes with 10 MBit/s
+        return {
+            'ara-clustercloud1': {
+                'ara-clustercloud1': 1.25e+9,
+                'ara-clustertegra1': 1.25e+6,
+                'ara-clusterpi1': 1.25e+6,
+                'ara-clusterpi2': 1.25e+6,
+                'ara-clusterpi3': 1.25e+6,
+                'ara-clusterpi4': 1.25e+6,
+                'registry': 1.25e+7
+            },
+            'ara-clustertegra1': {
+                'ara-clustercloud1': 1.25e+6,
+                'ara-clustertegra1': 1.25e+9,
+                'ara-clusterpi1': 1.25e+7,
+                'ara-clusterpi2': 1.25e+7,
+                'ara-clusterpi3': 1.25e+7,
+                'ara-clusterpi4': 1.25e+7,
+                'registry': 1.25e+7
+            },
+            'ara-clusterpi1': {
+                'ara-clustercloud1': 1.25e+6,
+                'ara-clustertegra1': 1.25e+7,
+                'ara-clusterpi1': 1.25e+9,
+                'ara-clusterpi2': 1.25e+7,
+                'ara-clusterpi3': 1.25e+7,
+                'ara-clusterpi4': 1.25e+7,
+                'registry': 1.25e+7
+            },
+            'ara-clusterpi2': {
+                'ara-clustercloud1': 1.25e+6,
+                'ara-clustertegra1': 1.25e+7,
+                'ara-clusterpi1': 1.25e+7,
+                'ara-clusterpi2': 1.25e+9,
+                'ara-clusterpi3': 1.25e+7,
+                'ara-clusterpi4': 1.25e+7,
+                'registry': 1.25e+7
+            },
+            'ara-clusterpi3': {
+                'ara-clustercloud1': 1.25e+6,
+                'ara-clustertegra1': 1.25e+7,
+                'ara-clusterpi1': 1.25e+7,
+                'ara-clusterpi2': 1.25e+7,
+                'ara-clusterpi3': 1.25e+9,
+                'ara-clusterpi4': 1.25e+7,
+                'registry': 1.25e+7
+            },
+            'ara-clusterpi4': {
+                'ara-clustercloud1': 1.25e+6,
+                'ara-clustertegra1': 1.25e+7,
+                'ara-clusterpi1': 1.25e+7,
+                'ara-clusterpi2': 1.25e+7,
+                'ara-clusterpi3': 1.25e+7,
+                'ara-clusterpi4': 1.25e+9,
+                'registry': 1.25e+7
+            }
+        }
