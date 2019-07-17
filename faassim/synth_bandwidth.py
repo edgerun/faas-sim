c1d34aabf4564d1fa42b1f216d1b53b277e315bf
Alexander Rashed <alexander.rashed@gmail.com>
2019-07-17 16:09:09 +0200

    Create bandwidth graph depending on the node type

--- faassim/synth_bandwidth.py
+++ faassim/synth_bandwidth.py
@@ -4,80 +4,35 @@ from typing import List
 from core.clustercontext import BandwidthGraph
 from core.model import Node
 
+# 1.25e+6 Byte/s = 10 MBit/s
+# 1.25e+7 Byte/s = 100 MBit/s
+# 1.25e+8 Byte/s = 1 GBit/s
+# 1.25e+9 Byte/s = 10 GBit/s - assumed for local access
+bandwidth_types = {
+    'edge': {
+        'edge': 1.25e+6,  # Edge-to-Edge: 10 MBit/s
+        'cloud': 1.25e+6  # Edge-to-Cloud: 10 MBit/s
+    },
+    'cloud': {
+        'edge': 1.25e+6,  # Cloud-to-Edge: 10 MBit/s
+        'cloud': 1.25e+8  # Cloud-to-Cloud: 1 GBit/s
+    }
+}
+
 
 def generate_bandwidth_graph(nodes: List[Node]) -> BandwidthGraph:
-    node_names = [node.name for node in nodes]
     bandwidth_graph: BandwidthGraph = defaultdict(lambda: defaultdict())
-    for node in node_names:
-        for inner in node_names:
-            bandwidth_graph[node][inner] = 1.25e+9
 
-    for node in node_names:
-        bandwidth_graph[node]['registry'] = 1.25e+9
-    '''
-    # TODO synthesize the graph -> N^2 entries!
-    #  for all nodes in self.list_nodes()
-    # 1.25e+6 Byte/s = 10 MBit/s
-    # 1.25e+7 Byte/s = 100 MBit/s
-    # 1.25e9 Byte/s = 10 GBit/s - assumed for local access
-    # The registry is always connected with 100 MBit/s (replicated in both networks)
-    # The edge nodes are interconnected with 100 MBit/s
-    # The cloud is connected to the edge nodes with 10 MBit/s
-    return {
-        'ara-clustercloud1': {
-            'ara-clustercloud1': 1.25e+9,
-            'ara-clustertegra1': 1.25e+6,
-            'ara-clusterpi1': 1.25e+6,
-            'ara-clusterpi2': 1.25e+6,
-            'ara-clusterpi3': 1.25e+6,
-            'ara-clusterpi4': 1.25e+6,
-            'registry': 1.25e+7
-        },
-        'ara-clustertegra1': {
-            'ara-clustercloud1': 1.25e+6,
-            'ara-clustertegra1': 1.25e+9,
-            'ara-clusterpi1': 1.25e+7,
-            'ara-clusterpi2': 1.25e+7,
-            'ara-clusterpi3': 1.25e+7,
-            'ara-clusterpi4': 1.25e+7,
-            'registry': 1.25e+7
-        },
-        'ara-clusterpi1': {
-            'ara-clustercloud1': 1.25e+6,
-            'ara-clustertegra1': 1.25e+7,
-            'ara-clusterpi1': 1.25e+9,
-            'ara-clusterpi2': 1.25e+7,
-            'ara-clusterpi3': 1.25e+7,
-            'ara-clusterpi4': 1.25e+7,
-            'registry': 1.25e+7
-        },
-        'ara-clusterpi2': {
-            'ara-clustercloud1': 1.25e+6,
-            'ara-clustertegra1': 1.25e+7,
-            'ara-clusterpi1': 1.25e+7,
-            'ara-clusterpi2': 1.25e+9,
-            'ara-clusterpi3': 1.25e+7,
-            'ara-clusterpi4': 1.25e+7,
-            'registry': 1.25e+7
-        },
-        'ara-clusterpi3': {
-            'ara-clustercloud1': 1.25e+6,
-            'ara-clustertegra1': 1.25e+7,
-            'ara-clusterpi1': 1.25e+7,
-            'ara-clusterpi2': 1.25e+7,
-            'ara-clusterpi3': 1.25e+9,
-            'ara-clusterpi4': 1.25e+7,
-            'registry': 1.25e+7
-        },
-        'ara-clusterpi4': {
-            'ara-clustercloud1': 1.25e+6,
-            'ara-clustertegra1': 1.25e+7,
-            'ara-clusterpi1': 1.25e+7,
-            'ara-clusterpi2': 1.25e+7,
-            'ara-clusterpi3': 1.25e+7,
-            'ara-clusterpi4': 1.25e+9,
-            'registry': 1.25e+7
-        }
-    }
-    '''
+    # Create the complete graph for the nodes with a bandwidth depending on the type
+    for node in nodes:
+        for inner in nodes:
+            bandwidth_graph[node.name][inner.name] = 1.25e+9 if node == inner else \
+            bandwidth_types[node.labels['locality.skippy.io/type']][inner.labels['locality.skippy.io/type']]
+
+    # Also add a connection from each node to the registry
+    for node in nodes:
+        # Bandwidth to the registry is 100 MBit/s for cloud, 10 MBit/s for edge
+        bandwidth = 1.25e+7 if node.labels['locality.skippy.io/type'] == 'cloud' else 1.25e+6
+        bandwidth_graph[node.name]['registry'] = bandwidth
+
     return bandwidth_graph
