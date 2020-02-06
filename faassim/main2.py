c16ed610096286be325e2ebfedf2682df03d1022
Thomas Rausch <thomas@rauschig.org>
2020-02-06 22:54:21 +0100

    Implement simulator data functions

--- faassim/main2.py
+++ faassim/main2.py
@@ -1,8 +1,8 @@
 import logging
+import os
 import time
 from typing import List
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -29,6 +29,7 @@ def extract_dataframe(measurement: str, records: List[Record]):
 
     df = pd.DataFrame(data)
     df.index = pd.DatetimeIndex(pd.to_datetime(df['time']))
+    del df['time']
     return df
 
 
@@ -44,51 +45,65 @@ class Simulation:
         self.env.process(self.env.faas_gateway.faas_idler())
         self.env.process(scenario.scenario_daemon(self.env))
 
+        self._data_frames = dict()
+
     def run(self, until=None):
         env = self.env
         env.run(until=until)
 
-        print(env.metrics.invocations)
+    def dataframe(self, metric):
+        if metric not in self._data_frames:
+            df = extract_dataframe(metric, self.env.metrics.records)
+            self._data_frames[metric] = df
 
-        df = extract_dataframe('invocations', self.env.metrics.records)
+        return self._data_frames[metric]
 
-        x = df['t_exec'].resample('10s').max()
-        plt.plot(x)
-        x = df['t_wait'].resample('10s').max()
-        plt.plot(x)
-        plt.show()
+    def dump_data_frames(self, directory='/tmp', prefix=None):
+        prefix = prefix or 'schedsim_%s_' % time.strftime('%y-%m-%d-%H-%M')
 
-        df['function_type'] = df['function_name'].str.split('_').str[2]
-        x = df[['function_type', 't_wait', 't_exec']]
+        metrics = {record.measurement for record in self.env.metrics.records}
 
-        counts = x.groupby('function_type')
+        for metric in metrics:
+            self.dataframe(metric).to_csv(os.path.join(directory, f'{prefix}{metric}.csv'))
 
-        for key, grp in counts:
-            y = grp['t_exec'].resample('60s').count()
-            plt.plot(y, label=key)
-        plt.title('average requests/minute')
-        plt.legend()
-        plt.show()
+    def calc_total_network(self):
+        return self.dataframe('network')['value'].sum() / 10e6
 
-        groups = x.groupby('function_type').mean()[['t_wait', 't_exec']]
-        print(groups)
+    def calc_average_fet(self):
+        df = self.dataframe('invocations')
+        df = df.dropna()
+        df['rtt'] = df['t_exec'] + df['t_wait']
 
-        df_alloc = extract_dataframe('allocation', self.env.metrics.records)
-        df_alloc = df_alloc[df_alloc['node'] == 'edge_9_2_tegra']
-        y = df_alloc['cpu']
-        plt.step(y.index, y)
-        plt.show()
+        return df['rtt'].mean()
 
-        y = df_alloc['mem']
-        plt.step(y.index, y)
-        plt.show()
+    def calc_edge_utilization(self):
+        df = self.dataframe('allocation')
+        nodes = [node.name for node in self.env.cluster.list_nodes()]
 
-        df_util = extract_dataframe('utilization', self.env.metrics.records)
-        df_util = df_util[df_util['node'] == 'edge_9_2_tegra']
-        y = df_util['mem'].resample('10s').mean().replace(np.nan, 0)
-        plt.ylim(0, 1)
-        plt.plot(y)
-        plt.show()
+        dfg = df.groupby('node').aggregate('mean')
+        dfg = dfg.reindex(nodes, fill_value=0)
+        dfg['zone'] = np.where(dfg.index.str.endswith('cloud'), 'cloud', 'edge')
+        dfg = dfg.groupby('zone').mean()
+
+        util_c = sum(dfg.loc['cloud'])
+        util_e = sum(dfg.loc['edge'])
+
+        return util_e / (util_e + util_c)
+
+    def calc_cloud_cost(self):
+        # TODO https://aws.amazon.com/lambda/pricing/
+        # https://aws.amazon.com/ec2/pricing/on-demand/ (Data Transfer)
+
+        # seconds = np.ceil(seconds + 0.05)
+        # total_compute_seconds = sum(np.ceil(seconds + 0.05))
+        # compute_gb_seconds = max(128, min(3072, ram)) * np.ceil(seconds + 0.05) # how much ram was allocated during execution
+        # compute_usd = compute_gb_seconds * 0.0000166667
+        # requests = count(invocations)
+        # request_usd = requests * 0.0000002
+
+        # return compute_usd * request_usd
+
+        raise NotImplementedError
 
 
 def main():
@@ -98,6 +113,8 @@ def main():
     sim.run(60 * 60)
     print('simulation took %.2f ms' % ((time.time() - then) * 1000))
     print('simulation time is now: %.2f' % sim.env.now)
+
+    sim.dump_data_frames()
 
 
 if __name__ == '__main__':
