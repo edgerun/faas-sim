adaee3d227c39708a807998f17b30e7e24fe9765
Thomas Rausch <thomas@rauschig.org>
2020-02-14 15:52:58 +0100

    Add experiment run to use optimized skippy parameters

--- faassim/main2.py
+++ faassim/main2.py
@@ -22,6 +22,18 @@ skippy_params = {
     'percentage_of_nodes_to_score': 100
 }
 
+weights = [6.45377, 4.78474, 8.99672, 8.92804, 1.23396]
+skippy_params_opt = {
+    'priorities': [
+        (weights[0], BalancedResourcePriority()),
+        (weights[1], LatencyAwareImageLocalityPriority()),
+        (weights[2], LocalityTypePriority()),
+        (weights[3], DataLocalityPriority()),
+        (weights[4], CapabilityPriority())
+    ],
+    'percentage_of_nodes_to_score': 100
+}
+
 kube_params_50 = {
     'priorities': [
         (1, BalancedResourcePriority()),
@@ -47,7 +59,7 @@ def run_sim(args):
     data_prefix = args[2].rstrip('_')
 
     logging.info('starting simulation %s with parameters %s', data_prefix, scheduler_parameters)
-    sim = Simulation(UrbanSensingScenario(), scheduler_parameters, faas_idler)
+    sim = Simulation(UrbanSensingScenario.lazy(), scheduler_parameters, faas_idler)
     then = time.time()
     sim.run()
     sim.dump_data_frames('/tmp/schedsim', prefix=data_prefix + '_')
@@ -58,12 +70,16 @@ def run_sim(args):
 def main():
     logging.basicConfig(level=logging.INFO)
 
+    logger.info('initializing scenario')
+    UrbanSensingScenario.lazy()
+
     params = {
         'skippy': skippy_params,
+        'skippyopt': skippy_params_opt,
         'kube50': kube_params_50,
         'kube100': kube_params_100,
     }
-    runs = 10
+    runs = 1
 
     arguments = []
     # with faas idler
