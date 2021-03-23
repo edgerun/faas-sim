46834fd4de2d666ed36b96c66abbe075b76c4e4f
Thomas Rausch <thomas@thrau.at>
2021-03-23 17:59:45 +0100

    Add custom_scheduler example and restructure basic example

--- examples/basic/main.py
+++ examples/basic/main.py
@@ -1,4 +1,5 @@
 import logging
+from typing import List
 
 import ether.scenarios.urbansensing as scenario
 from skippy.core.utils import parse_size_string
@@ -14,63 +15,19 @@ from sim.topology import Topology
 logger = logging.getLogger(__name__)
 
 
-class ExampleBenchmark(Benchmark):
+def main():
+    logging.basicConfig(level=logging.DEBUG)
 
-    def setup(self, env: Environment):
-        containers: docker.ContainerRegistry = env.container_registry
+    # a topology holds the cluster configuration and network topology
+    topology = example_topology()
 
-        # fill global container registry with images
-        containers.put(ImageProperties('smttest-cpu', parse_size_string('58M'), arch='arm32'))
-        containers.put(ImageProperties('smttest-cpu', parse_size_string('58M'), arch='x86'))
-        containers.put(ImageProperties('smttest-cpu', parse_size_string('58M'), arch='aarch64'))
+    # a benchmark is a simpy process that sets up the runtime system (e.g., creates container images, deploys functions)
+    # and creates workload by simulating function requests
+    benchmark = ExampleBenchmark()
 
-        containers.put(ImageProperties('resnet50-inference-cpu', parse_size_string('56M'), arch='arm32'))
-        containers.put(ImageProperties('resnet50-inference-cpu', parse_size_string('56M'), arch='x86'))
-        containers.put(ImageProperties('resnet50-inference-cpu', parse_size_string('56M'), arch='aarch64'))
-
-        containers.put(ImageProperties('resnet50-inference-gpu', parse_size_string('56M'), arch='arm32'))
-        containers.put(ImageProperties('resnet50-inference-gpu', parse_size_string('56M'), arch='x86'))
-        containers.put(ImageProperties('resnet50-inference-gpu', parse_size_string('56M'), arch='aarch64'))
-
-        for name, tag_dict in containers.images.items():
-            for tag, images in tag_dict.items():
-                logger.info('%s, %s, %s', name, tag, images)
-
-    def run(self, env: Environment):
-        deployments = self.prepare_deployments()
-
-        for deployment in deployments:
-            yield from env.faas.deploy(deployment)
-
-        logger.info('waiting for replica')
-        yield env.process(env.faas.poll_available_replica('smttest'))
-
-        # execute 10 requests in parallel
-        logger.info('executing requests')
-        ps = []
-        for i in range(16):
-            ps.append(env.process(env.faas.invoke(FunctionRequest('smttest'))))
-
-        # wait for invocation processes to finish
-        for p in ps:
-            yield p
-
-    def prepare_deployments(self):
-        smttest_deployment = FunctionDeployment(
-            name='smttest',
-            function_definitions={
-                'smttest-cpu': FunctionDefinition(name='smttest', image='smttest-cpu')
-            }
-        )
-        resnet50_inference_deployment = FunctionDeployment(
-            name='resnet50-inference',
-            function_definitions={
-                'resnet50-inference-gpu': FunctionDefinition(name='resnet50-inference', image='resnet50-inference-gpu'),
-                'resnet50-inference-cpu': FunctionDefinition(name='resnet50-inference', image='resnet50-inference-cpu')
-            }
-        )
-
-        return [smttest_deployment, resnet50_inference_deployment]
+    # a simulation runs until the benchmark process terminates
+    sim = Simulation(topology, benchmark)
+    sim.run()
 
 
 def example_topology() -> Topology:
@@ -81,15 +38,71 @@ def example_topology() -> Topology:
     return t
 
 
-def main():
-    logging.basicConfig(level=logging.DEBUG)
+class ExampleBenchmark(Benchmark):
 
-    # TODO: read experiment specification
-    topology = example_topology()
-    benchmark = ExampleBenchmark()
+    def setup(self, env: Environment):
+        containers: docker.ContainerRegistry = env.container_registry
 
-    sim = Simulation(topology, benchmark)
-    sim.run()
+        # populate the global container registry with images
+        containers.put(ImageProperties('python-pi-cpu', parse_size_string('58M'), arch='arm32'))
+        containers.put(ImageProperties('python-pi-cpu', parse_size_string('58M'), arch='x86'))
+        containers.put(ImageProperties('python-pi-cpu', parse_size_string('58M'), arch='aarch64'))
+
+        containers.put(ImageProperties('resnet50-inference-cpu', parse_size_string('56M'), arch='arm32'))
+        containers.put(ImageProperties('resnet50-inference-cpu', parse_size_string('56M'), arch='x86'))
+        containers.put(ImageProperties('resnet50-inference-cpu', parse_size_string('56M'), arch='aarch64'))
+
+        containers.put(ImageProperties('resnet50-inference-gpu', parse_size_string('56M'), arch='arm32'))
+        containers.put(ImageProperties('resnet50-inference-gpu', parse_size_string('56M'), arch='x86'))
+        containers.put(ImageProperties('resnet50-inference-gpu', parse_size_string('56M'), arch='aarch64'))
+
+        # log all the images in the container
+        for name, tag_dict in containers.images.items():
+            for tag, images in tag_dict.items():
+                logger.info('%s, %s, %s', name, tag, images)
+
+    def run(self, env: Environment):
+        # deploy functions
+        deployments = self.prepare_deployments()
+
+        for deployment in deployments:
+            yield from env.faas.deploy(deployment)
+
+        logger.info('waiting for replica')
+        yield env.process(env.faas.poll_available_replica('python-pi'))
+        yield env.process(env.faas.poll_available_replica('resnet50-inference'))
+
+        # run wokload
+        ps = []
+        # execute 10 requests in parallel
+        logger.info('executing 10 python-pi requests')
+        for i in range(10):
+            ps.append(env.process(env.faas.invoke(FunctionRequest('python-pi'))))
+
+        logger.info('executing 10 resnet50-inference requests')
+        for i in range(10):
+            ps.append(env.process(env.faas.invoke(FunctionRequest('resnet50-inference'))))
+
+        # wait for invocation processes to finish
+        for p in ps:
+            yield p
+
+    def prepare_deployments(self) -> List[FunctionDeployment]:
+        python_pi_deployment = FunctionDeployment(
+            name='python-pi',
+            function_definitions={
+                'python-pi-cpu': FunctionDefinition(name='python-pi', image='python-pi-cpu')
+            }
+        )
+        resnet50_inference_deployment = FunctionDeployment(
+            name='resnet50-inference',
+            function_definitions={
+                'resnet50-inference-gpu': FunctionDefinition(name='resnet50-inference', image='resnet50-inference-gpu'),
+                'resnet50-inference-cpu': FunctionDefinition(name='resnet50-inference', image='resnet50-inference-cpu')
+            }
+        )
+
+        return [python_pi_deployment, resnet50_inference_deployment]
 
 
 if __name__ == '__main__':
