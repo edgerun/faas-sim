a4698d00f288c985ff936750e9bcba206c25f3d6
Philipp <poepsi39@gmail.com>
2021-04-27 14:44:30 +0200

    Add analysis example

---
+++ examples/analysis/main.py
@@ -0,0 +1,37 @@
+import logging
+
+import examples.basic.main as basic
+from sim.faassim import Simulation
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    # prepare simulation with topology and benchmark from basic example
+    sim = Simulation(basic.example_topology(), basic.ExampleBenchmark())
+
+    # run the simulation
+    sim.run()
+
+    dfs = {
+        'invocations_df': sim.env.metrics.extract_dataframe('invocations'),
+        'scale_df': sim.env.metrics.extract_dataframe('scale'),
+        'schedule_df': sim.env.metrics.extract_dataframe('schedule'),
+        'replica_deployment_df': sim.env.metrics.extract_dataframe('replica_deployment'),
+        'function_deployments_df': sim.env.metrics.extract_dataframe('function_deployments'),
+        'function_deployment_df': sim.env.metrics.extract_dataframe('function_deployment'),
+        'function_deployment_lifecycle_df': sim.env.metrics.extract_dataframe('function_deployment_lifecycle'),
+        'functions_df': sim.env.metrics.extract_dataframe('functions'),
+        'flow_df': sim.env.metrics.extract_dataframe('flow'),
+        'network_df': sim.env.metrics.extract_dataframe('network'),
+        'utilization_df': sim.env.metrics.extract_dataframe('utilization'),
+        'fets_df': sim.env.metrics.extract_dataframe('fets')
+    }
+
+    logger.info('Mean exec time %d', dfs['invocations_df']['t_exec'].mean())
+
+
+if __name__ == '__main__':
+    main()
