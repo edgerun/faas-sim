ac368e7b679673f6c0fe9edc67e5422bc98282a3
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2022-08-30 16:18:27 +0100

    Fix recursive property invocation and remove local DeploymentRanking as this is provided from the faas project.

--- sim/context/platform/deployment/model.py
+++ sim/context/platform/deployment/model.py
@@ -1,6 +1,6 @@
-from typing import List, Dict
+from typing import List
 
-from faas.system import FunctionDeployment, FunctionContainer, Function, ScalingConfiguration
+from faas.system import FunctionDeployment, FunctionContainer, Function, ScalingConfiguration, DeploymentRanking
 
 
 class SimScalingConfiguration:
@@ -25,7 +25,7 @@ class SimScalingConfiguration:
 
     @property
     def scale_factor(self):
-        return self.scale_factor
+        return self.scaling_config.scale_factor
 
     # average requests per second threshold for scaling
     rps_threshold: int = 20
@@ -47,26 +47,9 @@ class SimScalingConfiguration:
 
     target_average_rps_threshold = 0.1
 
+    def __str__(self):
+        return str(self.scaling_config)
 
-class DeploymentRanking:
-    # TODO probably better to remove default/enable default for one image
-    images: List[str]
-
-    # TODO probably removable after moving decision on which node to deploy pod to user
-    # percentages of scaling per image, can be used to hinder scheduler to overuse expensive resources (i.e. tpu)
-    function_factor: Dict[str, float]
-
-    def __init__(self, images: List[str], function_factor: Dict[str, float] = None):
-        self.images = images
-        self.function_factor = function_factor if function_factor is not None else {image: 1 for image in images}
-
-    def set_first(self, image: str):
-        index = self.images.index(image)
-        updated = self.images[:index] + self.images[index + 1:]
-        self.images = [image] + updated
-
-    def get_first(self):
-        return self.images[0]
 
 
 class SimFunctionDeployment(FunctionDeployment):
@@ -79,7 +62,7 @@ class SimFunctionDeployment(FunctionDeployment):
         super().__init__(fn, fn_containers, scaling_config, deployment_ranking)
         self.scaling_config = scaling_config
         if deployment_ranking is None:
-            self.ranking = DeploymentRanking([x.image for x in self.fn.fn_images])
+            self.ranking = DeploymentRanking(fn_containers)
         else:
             self.ranking = deployment_ranking
 
