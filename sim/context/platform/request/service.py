399259387c62690aeee0e210ac8654d2674216f3
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2022-09-06 14:03:33 +0100

    Implement get_inflight_request method

--- sim/context/platform/request/service.py
+++ sim/context/platform/request/service.py
@@ -22,7 +22,7 @@ class SimpleRequestService(RequestService):
         self.rwlock = ReadWriteLock()
         self.get_time = get_time
         # TODO this will leak memory, implement clean up to delete requests that are no relevant anymore
-        #      not relevant means that there is no intersection with a running one anymore on the node
+        #      not relevant means that we need to introduce a timeout argument
         self.requests: Dict[Union[str, int], FunctionRequest] = {}
         self.end_ts: Dict[Union[str, int], float] = {}
         self.start_ts: Dict[Union[str, int], float] = {}
@@ -47,3 +47,20 @@ class SimpleRequestService(RequestService):
     def get_end_ts(self, request_id: Union[str, int]) -> Optional[float]:
         with self.rwlock.lock.gen_rlock():
             return self.end_ts.get(request_id)
+
+    def get_inflight_request(self, node: str, now: float = None) -> List[FunctionRequest]:
+        if now is None:
+            now = self.get_time()
+        requests = []
+        for key, request in self.requests.items():
+            if self.end_ts.get(key) is None or self.end_ts[key] == now:
+                # trace has not ended or has ended in the same moment
+                requests.append(request)
+                continue
+            if self.end_ts[key] > now:
+                # invoker of method has passed a time that is in the past
+                # thus the end_ts can be set and lie in the future from now
+                if self.start_ts[key] < now:
+                    # make sure that we only include traces that have started before now
+                    requests.append(request)
+        return requests
