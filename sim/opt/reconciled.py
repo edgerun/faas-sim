b8c6972aa54740519e89786f95a4f7defaa609ed
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2022-09-08 16:31:20 +0100

    Log duration of optimizer

--- sim/opt/reconciled.py
+++ sim/opt/reconciled.py
@@ -1,8 +1,11 @@
+import time
+
 from faas.opt.api import Optimizer
 from faas.opt.reconciled import ReconciliationOptimizationDaemon
 
 from sim.core import Environment
-
+import logging
+logger = logging.getLogger(__name__)
 
 class SimReconciliationOptimizationDaemon(ReconciliationOptimizationDaemon):
 
@@ -18,7 +21,9 @@ class SimReconciliationOptimizationDaemon(ReconciliationOptimizationDaemon):
         self.optimizer.run()
         while self.is_running:
             yield from self.sleep()
+            start = time.time()
             yield from self.optimizer.run()
+            logger.info(f'Optimizer ran for {time.time() - start}')
 
 
 
