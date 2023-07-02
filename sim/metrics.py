693c85cb308857caf14a435f301756d6bd9eea2a
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2023-07-02 13:16:57 -0700

    Implement FlushingLogger

--- sim/metrics.py
+++ sim/metrics.py
@@ -1,17 +1,101 @@
+import glob
+import os.path
 from collections import defaultdict
+from pathlib import Path
 from typing import Dict
 
 import pandas as pd
 from ether.core import Capacity
-from faas.system import Metrics, RuntimeLogger, NullLogger
 from faas.system.core import FunctionRequest
 from skippy.core.clustercontext import ClusterContext
 from skippy.core.model import SchedulingResult
 
+from faas.system import Metrics, RuntimeLogger, NullLogger, MetricsLogger, WallClock, Record
 from sim.context.platform.deployment.model import SimFunctionDeployment
 from sim.core import Environment
 from sim.faas import SimFunctionReplica
-from sim.resource import ResourceUtilization
+
+
+class FlushingRuntimeLogger(MetricsLogger):
+    """This implementation continuously extracts all data into dataframes and flushes the recorded
+    logs. While it is not possible extract the dataframes after the simulation finished, it increases the scalability of the simulation by keeping the memory usage constant for the metrics collection."""
+
+    def __init__(self, results_folder: str, buffer_size: int = 100, clock=None) -> None:
+        self.results_folder = results_folder
+        self._init_results_folder()
+        self.records = list()
+        self.buffer_size = buffer_size
+        self.clock = clock or WallClock()
+
+    def _init_results_folder(self):
+        Path(self.results_folder).mkdir(parents=True, exist_ok=False)
+
+    def get(self, name, **tags):
+        return lambda x: self.log(name, x, None, **tags)
+
+    def log(self, metric, value, time=None, **tags):
+        """
+        Call l.log('cpu_load', .65, host='server0', region='us-west') or
+
+        :param metric: the name of the measurement
+        :param value: the measurement value
+        :param time: the (optional) time, otherwise now will be used
+        :param tags: additional tags describing the measurement
+        :return:
+        """
+        if time is None:
+            time = self._now()
+
+        if type(value) == dict:
+            fields = value
+        else:
+            fields = {
+                'value': value
+            }
+
+        self._store_record(Record(metric, time, fields, tags))
+
+    def _store_record(self, record: Record):
+        self.records.append(record)
+        if len(self.records) > self.buffer_size:
+            self._flush()
+
+    def _flush(self):
+        measurement_keys = []
+        new_measurement_dfs = {}
+        old_measurement_dfs = self._read_existing_dfs()
+
+        for measurement in measurement_keys:
+            df = extract_dataframe(self.records, measurement)
+            if old_measurement_dfs is not None:
+                old_df = old_measurement_dfs[measurement]
+                new_measurement_dfs[measurement] = pd.concat([old_df, df])
+            else:
+                new_measurement_dfs[measurement] = df
+
+        self._save_measurements(new_measurement_dfs)
+        del self.records
+        self.records = list()
+
+    def _save_measurements(self, measurement_dfs: Dict[str, pd.DataFrame]):
+        for measurement, df in measurement_dfs.items():
+            file_path = f'{self.results_folder}/{measurement}.csv'
+            df.to_csv(file_path)
+
+    def _read_existing_dfs(self):
+        filenames = glob.glob(f'{self.results_folder}/*.csv')
+        if len(filenames) == 0:
+            return None
+        dfs = {}
+        for filename in filenames:
+            df = pd.read_csv(filename)
+            measurement_key = os.path.basename(filename).replace('.csv', '')
+            dfs[measurement_key] = df
+
+        return dfs
+
+    def _now(self):
+        return self.clock.now()
 
 
 class SimMetrics(Metrics):
@@ -218,99 +302,104 @@ class SimMetrics(Metrics):
     def records(self):
         return self.logger.records
 
-    def _get_traces(self):
-        requests_by_id = defaultdict(list)
-        for record in self.records:
-            if record.measurement == 'invocations':
-                for k, v in record.fields.items():
-                    if k == 'request_id':
-                        requests_by_id[v].append(record)
-                for k, v in record.tags.items():
-                    if k == 'request_id':
-                        requests_by_id[v].append(record)
-        data = list()
-
-        for request_id, records in requests_by_id.items():
-            max_rtt = 0
-            max_response = None
-            last_start = 0
-            last_response = None
-            for record in records:
-                if record.fields['t_exec'] > max_rtt:
-                    # this is the invocation of the client to load balancer
-                    max_rtt = record.fields['t_exec']
-                    max_response = record
-                if record.fields['ts_start'] > last_start:
-                    # this is the last invocation from load balancer to actual replica
-                    last_start = record.fields['ts_start']
-                    last_response = record
-            t_wait = max_response.fields['t_wait']
-            ts_received = max_response.fields['ts_received']
-            t_exec = last_response.fields['t_exec']
-            ts_end = last_response.fields['ts_end']
-            ts_start = max_response.fields['ts_start']
-            memory = last_response.fields['memory']
-            status = max_response.fields['status']
-            if max_response.fields.get('client'):
-                client = max_response.fields['client']
-            else:
-                client = 'N/A'
-
-            function_name = last_response.tags['function_name']
-            function_image = last_response.tags['function_image']
-            node = last_response.tags['node']
-            replica_id = last_response.tags['replica_id']
-            request_id = last_response.tags['request_id']
-
-            r = dict()
-            r['time'] = max_response.time
-            r['t_wait'] = t_wait
-            r['ts_received'] = ts_received
-            r['t_exec'] = t_exec
-            r['ts_end'] = ts_end
-            r['ts_start'] = ts_start
-            r['memory'] = memory
-            r['status'] = status
-            r['client'] = client
-            r['function_name'] = function_name
-            r['function_image'] = function_image
-            r['node'] = node
-            r['replica_id'] = replica_id
-            r['request_id'] = request_id
-            data.append(r)
-
-        df = pd.DataFrame(data)
-
-        if len(data) == 0:
-            return df
-
-        df.index = pd.DatetimeIndex(pd.to_datetime(df['time']))
-        del df['time']
-        return df
-
     def extract_dataframe(self, measurement: str):
-        if measurement == 'traces':
-            return self._get_traces()
+        return extract_dataframe(self.records, measurement)
 
-        data = list()
 
-        for record in self.records:
-            if record.measurement != measurement:
-                continue
+def extract_dataframe(records, measurement: str):
+    if measurement == 'traces':
+        return _get_traces(records)
 
-            r = dict()
-            r['time'] = record.time
-            for k, v in record.fields.items():
-                r[k] = v
-            for k, v in record.tags.items():
-                r[k] = v
+    data = list()
 
-            data.append(r)
-        df = pd.DataFrame(data)
+    for record in records:
+        if record.measurement != measurement:
+            continue
 
-        if len(data) == 0:
-            return df
+        r = dict()
+        r['time'] = record.time
+        for k, v in record.fields.items():
+            r[k] = v
+        for k, v in record.tags.items():
+            r[k] = v
 
-        df.index = pd.DatetimeIndex(pd.to_datetime(df['time']))
-        del df['time']
+        data.append(r)
+    df = pd.DataFrame(data)
+
+    if len(data) == 0:
         return df
+
+    df.index = pd.DatetimeIndex(pd.to_datetime(df['time']))
+    del df['time']
+    return df
+
+
+def _get_traces(records):
+    requests_by_id = defaultdict(list)
+    for record in records:
+        if record.measurement == 'invocations':
+            for k, v in record.fields.items():
+                if k == 'request_id':
+                    requests_by_id[v].append(record)
+            for k, v in record.tags.items():
+                if k == 'request_id':
+                    requests_by_id[v].append(record)
+    data = list()
+
+    for request_id, records in requests_by_id.items():
+        max_rtt = 0
+        max_response = None
+        last_start = 0
+        last_response = None
+        for record in records:
+            if record.fields['t_exec'] > max_rtt:
+                # this is the invocation of the client to load balancer
+                max_rtt = record.fields['t_exec']
+                max_response = record
+            if record.fields['ts_start'] > last_start:
+                # this is the last invocation from load balancer to actual replica
+                last_start = record.fields['ts_start']
+                last_response = record
+        t_wait = max_response.fields['t_wait']
+        ts_received = max_response.fields['ts_received']
+        t_exec = last_response.fields['t_exec']
+        ts_end = last_response.fields['ts_end']
+        ts_start = max_response.fields['ts_start']
+        memory = last_response.fields['memory']
+        status = max_response.fields['status']
+        if max_response.fields.get('client'):
+            client = max_response.fields['client']
+        else:
+            client = 'N/A'
+
+        function_name = last_response.tags['function_name']
+        function_image = last_response.tags['function_image']
+        node = last_response.tags['node']
+        replica_id = last_response.tags['replica_id']
+        request_id = last_response.tags['request_id']
+
+        r = dict()
+        r['time'] = max_response.time
+        r['t_wait'] = t_wait
+        r['ts_received'] = ts_received
+        r['t_exec'] = t_exec
+        r['ts_end'] = ts_end
+        r['ts_start'] = ts_start
+        r['memory'] = memory
+        r['status'] = status
+        r['client'] = client
+        r['function_name'] = function_name
+        r['function_image'] = function_image
+        r['node'] = node
+        r['replica_id'] = replica_id
+        r['request_id'] = request_id
+        data.append(r)
+
+    df = pd.DataFrame(data)
+
+    if len(data) == 0:
+        return df
+
+    df.index = pd.DatetimeIndex(pd.to_datetime(df['time']))
+    del df['time']
+    return df
